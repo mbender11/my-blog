73a1ca8016d5b88526b7af2b29ea4a93b444c726
Mason Bender <mbender@captechconsulting.com>
2020-08-10 11:02:31 -0600

    first commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+        <h1>About Me</h1>
+        <p>Welcome to my blog! blhawfk jnweflui hawefui hawlfhawif jhawliefawliefhawliefuh wwadsal</p>
+    </>
+);
+
+export default AboutPage;
